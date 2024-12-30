068f8b31334a4dbb37562bb0126e5d1b6ddbdc10
tr83 <terorii@gmail.com>
2024-12-30 20:34:00 +0200

    block_to_block_type function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,8 +1,19 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    HEADING = 'heading'
+    PARAGRAPH = 'paragraph'
+    CODE = 'code'
+    QUOTE = 'quote'
+    UNORDERED_LIST = 'unordered_list'
+    ORDERED_LIST = 'ordered_list'
+
 def markdown_to_blocks(markdown):
     raw_blocks = markdown.split("\n\n")
     blocks = []
 
-    for i, raw_block in enumerate(raw_blocks):
+    for raw_block in raw_blocks:
         stripped_block = raw_block.strip()
         if stripped_block == '' or stripped_block == '\n':
             continue
@@ -22,19 +33,34 @@ def markdown_to_blocks(markdown):
 
     return blocks
 
-# Remove and finish the section with tests
-def main():
-    markdown = """# This is a heading
-
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
-
+def block_to_block_type(markdown):
+    if markdown.startswith('# ') or markdown.startswith('## ') or markdown.startswith('### ') or markdown.startswith('#### ') or markdown.startswith('##### ') or markdown.startswith('###### '):
+        return BlockType.HEADING.value
+    elif markdown.startswith('```') and markdown.endswith('```'):
+        return BlockType.CODE.value
     
+    lines = markdown.split('\n')
 
-   * This is the first list item in a list block
-* This is a list item
-* This is another list item"""
-    blocks = markdown_to_blocks(markdown)
-    for block in blocks:
-        print(block)
+    type = None
+    for line in lines:
+        if (type == None or type == BlockType.QUOTE.value) and line.startswith('>'):
+            type = BlockType.QUOTE.value
+        elif (type == None or type == BlockType.UNORDERED_LIST.value) and (line.startswith('* ') or line.startswith('- ')):
+            type = BlockType.UNORDERED_LIST.value
+        elif re.match(r'^\d+\.', line):
+            type = BlockType.ORDERED_LIST.value
+            # Validate the ordered list block
+            for j, line in enumerate(lines):
+                expected_number = j + 1
+                if not re.match(rf'^{expected_number}\. ', line):
+                    type = BlockType.PARAGRAPH.value
+            if type == BlockType.ORDERED_LIST.value:
+                # Correct the ordered list block
+                for j, line in enumerate(lines):
+                    expected_number = j + 1
+                    lines[j] = f'{expected_number}. ' + line.lstrip('0123456789. ')
+                markdown = '\n'.join(lines)
+        else:
+            type = BlockType.PARAGRAPH.value
 
-main()
+    return type
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -54,3 +54,95 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
             'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
             '''* This is the first list item in a list block\n* This is a list item\n* This is another list item'''
         ])
+
+    def test_block_to_block_type_heading(self):
+        text = '# This is a heading'
+        self.assertEqual(block_to_block_type(text), 'heading')
+        text = '## This is a heading'
+        self.assertEqual(block_to_block_type(text), 'heading')
+        text = '### This is a heading'
+        self.assertEqual(block_to_block_type(text), 'heading')
+        text = '#### This is a heading'
+        self.assertEqual(block_to_block_type(text), 'heading')
+        text = '##### This is a heading'
+        self.assertEqual(block_to_block_type(text), 'heading')
+        text = '###### This is a heading'
+        self.assertEqual(block_to_block_type(text), 'heading')
+
+    def test_block_to_block_type_invalid_heading(self):
+        text = '#This will end up being a paragraph'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+
+    def test_block_to_block_type_code(self):
+        text = '```This is a code block```'
+        self.assertEqual(block_to_block_type(text), 'code')
+        text = '```    This is a code block    ```'
+        self.assertEqual(block_to_block_type(text), 'code')
+        text = '```This is a code block\n`With multiple lines`\nand more than three ending characters````````'
+        self.assertEqual(block_to_block_type(text), 'code')
+
+    def test_block_to_block_type_invalid_code(self):
+        text = ' ```This will end up being a paragraph``` '
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '``This will end up being a paragraph```'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '```This will end up being a paragraph```.'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+
+    def test_block_to_block_type_quote(self):
+        text = '>This is a quote'
+        self.assertEqual(block_to_block_type(text), 'quote')
+        text = '> This is a quote'
+        self.assertEqual(block_to_block_type(text), 'quote')
+        text = '> This is a quote\n> with multiple lines'
+        self.assertEqual(block_to_block_type(text), 'quote')
+    
+    def test_block_to_block_type_invalid_quote(self):
+        text = ' >This will end up being a paragraph'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '> This will end up being a paragraph\nThis is not a quote'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+
+    def test_block_to_block_type_unordered_list(self):
+        text = '* This is an unordered list item'
+        self.assertEqual(block_to_block_type(text), 'unordered_list')
+        text = '- This is an unordered list item'
+        self.assertEqual(block_to_block_type(text), 'unordered_list')
+        text = '* This is an unordered list item\n* Another item'
+        self.assertEqual(block_to_block_type(text), 'unordered_list')
+        text = '- This is an unordered list item\n- Another item'
+        self.assertEqual(block_to_block_type(text), 'unordered_list')
+
+    def test_block_to_block_type_invalid_unordered_list(self):
+        text = ' *This will end up being a paragraph'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '*This will end up being a paragraph'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '* This will end up being a paragraph\nThis is not a list item'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '-This will end up being a paragraph'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+
+    def test_block_to_block_type_ordered_list(self):
+        text = '1. This is an ordered list item'
+        self.assertEqual(block_to_block_type(text), 'ordered_list')
+        text = '1. This is an ordered list item\n2. Another item'
+        self.assertEqual(block_to_block_type(text), 'ordered_list')
+        text = '1. This is an ordered list item\n2. Another item\n3. Yet another item'
+        self.assertEqual(block_to_block_type(text), 'ordered_list')
+
+    def test_block_to_block_type_invalid_ordered_list(self):
+        text = '1.This will end up being a paragraph'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '1. This will end up being a paragraph\n2.This is not a list item'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '1. This will end up being a paragraph\n3. This is not a list item'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = '1. This will end up being a paragraph\n2 This is not a list item'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+
+    def test_block_to_block_type_paragraph(self):
+        text = 'This is a paragraph of text.'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+        text = 'This is a paragraph\nwith multiple lines.'
+        self.assertEqual(block_to_block_type(text), 'paragraph')
