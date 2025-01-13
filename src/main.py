5a6313d8f1563dcd4324f35a7a072ec09e921c3f
tr83 <terorii@gmail.com>
2025-01-13 22:09:19 +0200

    Main function to do 'static' -> 'public' file copying

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_source_to_destination(source, destination):
+    print(f'Source: {source} -> Destination: {destination}')
+
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    
+    shutil.copytree(source, destination)
 
 def main():
-    textNode = TextNode('Test text', TextType.BOLD, 'https://www.boot.dev')
-    print(textNode)
+    copy_source_to_destination('static', 'public')
 
 main()
